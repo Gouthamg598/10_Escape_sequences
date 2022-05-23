55d412e30cb3f91631afb2a3507e25a186afcd79
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-23 18:54:00 +0530

    Add files via upload

---
+++ escape.py
@@ -0,0 +1,65 @@
+'''escape sequences'''
+
+
+
+# hi\arjun        \a --> alert
+
+# print('hi\arjun') #hirjun
+# print('hi\\arjun')#hi\arjun
+
+#  \b--->backspace
+
+# hi\bharth   
+# print('hi\bharth')#hharth
+# print('hi \bharth')#hiharth
+# print('hi  \bharth')#hi harth
+# print('hi\\bharath') hi\bharath
+
+'''c to e dont have meaning'''
+
+# print('hi\cat')
+# print('hi\charan')
+# print('hi\dinesh')
+# print('hi\eshwar')
+
+# print('hi\farooq') #we get hi♀arooq, symbol which is like female or new line # hi
+# print('hi\\farooq') 
+# print('hi\fan')      #hi♀an                                                             
+# print('hi\\fan')      #hi♀an                                                             
+
+#from g to m has no meaning
+
+# print('hi\nHello')
+# print('hi\\nHello')hi\nHello
+# print('hi\n Hello') #used for next line or new line
+'''o,p,q are have no meaning'''
+
+# print('fruit\orange')
+# print('bird\parrot')
+# print('we are in\queee')
+
+'''\r --> carriage return'''
+'''if right side of \r have more characters then right side of \r will printed '''
+# print('hi\rahul')#ahul
+# print('hi\\rahul') #hi\rahul
+'''if both sides of \r is same noof characters.right side of \r will printed'''
+# print('hai\ramu') #amu
+# print('hai\\ramu')#hai\ramu
+'''if left side has more letters the letters which are at right side will be replaces the left side letters and continue'''
+# print('hello\ramu')  #amulo
+# print('hello\ram') amllo
+# print('hello\\ramu')  #hello\ramu
+# print('hi\tharun')# no of spaces are different for diiferent statements so \t not used for coding
+
+# print('hi\usha')#unicode error
+# print('hi\\usha')#hi\usha
+
+# print('hi\varun ♂')#male symbol
+# print('hi\\varun ♂')#male symbol
+
+# print('hi\warner')#no meaning
+
+#print('hi\xavier')# unicode error
+# print('hi\\xavier')#hi\xavier
+# print('hi\yellow')
+# print('hi\zoo')
